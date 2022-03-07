07b18ecb9866ede238886b4f70f1145dbeda1632
Varlamov Alexander <91311709+Alphonsce@users.noreply.github.com>
2022-03-07 16:32:35 +0300

    finding m maximums from n elements added

---
+++ CPP_labs/m_max.cpp
@@ -0,0 +1,60 @@
+Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void print_array(float *arr, int size)
+{
+    for (int i = 0; i < size; i++) {
+        std::cout << *(arr + i) << ' ';
+    }
+    std::cout << std::endl;
+}
+
+void move_array_one_forward_starting_from_pivot(float *arr, int size, int pivot)
+{
+    for (int i = size - 1; i > pivot; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+}
+
+int main()
+{
+    int n, m;
+    float a; 
+    std::cout << "Введите длину потока ввода" << std::endl;
+    std::cin >> n;
+    std::cout << "Введите количество максимумов, которое необходимо вывести" << std::endl;
+    std::cin >> m;
+
+    float max[m];
+    for (int i = 0; i < m; i++)
+    {
+        std::cin >> a;
+        max[i] = a;
+    }
+    
+    std::sort(max, max + m, std::greater<float>());
+// массив сортируется к виду 5 4 3 2 1
+// 3 2 1 ---> a 3 2
+    for (int i = 0 ;i < n - m; i++)
+    {
+        std::cin >> a;    
+        for (int j = 0; j < m; j++)
+        {
+            if (a >= max[j])
+            {
+                move_array_one_forward_starting_from_pivot(max, m, j);
+                max[j] = a;
+                break;  
+            }
+        }
+    }
+
+    print_array(max, m);
+    return 0;
+}
