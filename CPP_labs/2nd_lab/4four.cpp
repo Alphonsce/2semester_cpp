82b712bc4e44d03c201698fe1026861ef2ba587e
Alphonsce <varlamov.al@phystech.edu>
2022-03-16 00:16:02 +0300

    2 new formulas added

--- CPP_labs/2nd_lab/4four.cpp
+++ CPP_labs/2nd_lab/4four.cpp
@@ -3,9 +3,31 @@
 #include <cstdlib>
 #include <cmath>
 
-const float PI = 3.141592653589793;
+const double PI = 3.141592653589793;
 
-short int minus_one_to_n(unsigned n) {
+const double one_over_sqrt_two = 0.707106781186547524401;
+const double ramanujans_const = 0.0002885855652225477091;
+
+// Реализация факториала с хвостовой рекурсией:
+unsigned fact(unsigned n) {
+    unsigned fac_n = 1;
+    if (n == 0 || n == 1)
+        return 1;
+    for (int i = 1; i <= n; i ++) {
+        fac_n *= i;
+    }
+    return fac_n;
+}
+
+unsigned power(unsigned n, unsigned p) {
+    unsigned result = 1;
+    for (int i = 0; i < p; i ++) {
+        result *= n;
+    }
+    return result;
+}
+
+short int minus_one_to_n(long long int n) {
     if (n % 2 == 0)
         return 1;
     return -1;
@@ -29,7 +51,8 @@ float leibnitz_pi(float precision, long unsigned max_iter) {
     return pi_4 * 4;
 }
 
-float euler_pi(float precision, unsigned max_iter) {
+float euler_pi(float precision, long unsigned max_iter) {
+    // 3.14139|3 - лучшая точность
     float pi_sq_6 = 0.;
     long unsigned step = 0;
     long unsigned unsquared_delimeter = 1;
@@ -45,11 +68,57 @@ float euler_pi(float precision, unsigned max_iter) {
     return pi;
 }
 
+double gauss_legendre(float tol, unsigned iterations) {
+    // лучшее значение: 3.14159265358979
+    double a_next, b_next, t_next, p_next;
+    double a = 1;
+    double b = one_over_sqrt_two;
+    double t = 0.25;
+    unsigned p = 1;
+    double pi = 0;
+    
+    long unsigned i = 0;
+    do {
+        a_next = 0.5 * (a + b);
+        b_next = sqrt(a * b);
+        t_next = t - p * (a_next - a) * (a_next - a);
+        p = (p << 1);
+        pi = (a_next + b_next) * (a_next + b_next) / (4 * t_next);
+        if (i >= iterations)
+            break;
+        a = a_next;
+        b = b_next;
+        t = t_next;
+        i ++;
+    } while (std::abs(pi - PI) >  tol);
+    return pi;
+}
+
+double ramanujan_pi(float tol, unsigned iterations) {
+    double one_over_pi = 0;
+    double numerator, delimeter;
+    unsigned k = 0;
+    do {
+        unsigned fac_k = fact(k);
+        numerator = fact(4 * k) * (1103 + 26390 * k);
+        delimeter = power(fac_k, 4) * power(396, 4 * k);
+        one_over_pi += numerator / delimeter;
+        std::cout << numerator << std::endl; 
+        std::cout << delimeter << std::endl;
+        std::cout << "--------------------" << std::endl;  
+        if (k >= iterations)
+            break;
+        k ++;
+    } while(std::abs(1. / one_over_pi - PI) > tol);
+    return 1. / (one_over_pi * ramanujans_const);
+}
+
 int main() {
     std::cout << std::fixed;
-    std::cout.precision(10);
+    std::cout.precision(20);
 
-    std::cout << euler_pi (0, 500) << std::endl;
+    // std::cout << gauss_legendre(0, 5) << std::endl;
+    std::cout << ramanujan_pi(0, 4) << std::endl; 
 
     return 0;
 }
