fe38571ed84facd63d9cd067f860b24f64caabae
Alphonsce <varlamov.al@phystech.edu>
2022-03-21 22:18:04 +0300

    lab is done

--- CPP_labs/2nd_lab/4four_about_pi/includes/dig_calculation.cpp
+++ CPP_labs/2nd_lab/4four_about_pi/includes/dig_calculation.cpp
@@ -7,15 +7,14 @@
 
 #include "utility_foos.hpp"
 #include "constants.hpp"
-#include "dig_calculation.hpp"
 
 void dig_leibnitz_pi(long unsigned max_iter, std::string output_path) {
     // наилучший результат: 3.141592
     float pi_4 = 0;
-    float pi;
+    float pi = 0;
     long unsigned delimeter = 1;
     unsigned digits = 1;        // number of digits after the dot
-    long double tol = pow(10., -digits);
+    long double tol = pow(10., -1. * digits);
 
     std::ofstream myfile;
     myfile.open(output_path);
@@ -29,140 +28,158 @@ void dig_leibnitz_pi(long unsigned max_iter, std::string output_path) {
         if (std::abs(pi - PI) < tol) {
             myfile << i + 1 << ',' << digits << std::endl;
             digits += 1;
-            tol = pow(10., -digits);
+            tol = pow(10., -1. * digits);
         }
     }
 }
 
-// void dig_euler_pi(long unsigned max_iter, std::string output_path) {
-//     // 3.14139|3 - лучшая точность
-//     float pi_sq_6 = 0.;
-//     long unsigned step = 0;
-//     long unsigned unsquared_delimeter = 1;
+void dig_euler_pi(long unsigned max_iter, std::string output_path) {
+    float pi_sq_6 = 0.;
+    float pi = 0;
+    long unsigned unsquared_delimeter = 1;
+    unsigned digits = 1;        // number of digits after the dot
+    long double tol = pow(10., -1. * digits);
 
-//     std::ofstream myfile;
-//     myfile.open(output_path);
-//     myfile << "iter,pi,tol\n";
-//     myfile.precision(20);
+    std::ofstream myfile;
+    myfile.open(output_path);
+    myfile << "iter,digits\n";
+    myfile.precision(20);
 
-//     while (std::abs( sqrt(6 * pi_sq_6) - PI) > precision) {
-//         pi_sq_6 += 1. / (unsquared_delimeter * unsquared_delimeter);
-//         unsquared_delimeter += 1;
-//         step += 1;
+    for (int i = 0; i < max_iter; i++) {
+        pi_sq_6 += 1. / (unsquared_delimeter * unsquared_delimeter);
+        unsquared_delimeter += 1;
+        pi = sqrt(6 * pi_sq_6);
+        if (std::abs(pi - PI) < tol) {
+            myfile << i + 1 << ',' << digits << std::endl;
+            digits += 1;
+            tol = pow(10., -1. * digits);
+        }
+    }
+}
 
-//         myfile << step + 1 << ',' <<  sqrt(6 * pi_sq_6) << ',' << precision << std::endl;
-//         if (step >= max_iter) {
-//             break;
-//         }
-//     }
-//     float pi = sqrt(6 * pi_sq_6);
-// }
+void dig_gauss_legendre_pi(long unsigned iterations, std::string output_path) {
+    // лучшее значение: 3.14159265358979
+    double a_next, b_next, t_next, p_next;
+    double a = 1;
+    double b = one_over_sqrt_two;
+    double t = 0.25;
+    unsigned p = 1;
+    double pi = 0;
 
-// void dig_gauss_legendre_pi(long unsigned iterations, std::string output_path) {
-//     // лучшее значение: 3.14159265358979
-//     double a_next, b_next, t_next, p_next;
-//     double a = 1;
-//     double b = one_over_sqrt_two;
-//     double t = 0.25;
-//     unsigned p = 1;
-//     double pi = 0;
+    unsigned digits = 1;        // number of digits after the dot
+    long double tol = pow(10., -1. * digits);
+
+    std::ofstream myfile;
+    myfile.open(output_path);
+    myfile << "iter,digits\n";
+    myfile.precision(20);
+
+    for (int i = 0; i < iterations; i++) {
+        a_next = 0.5 * (a + b);
+        b_next = sqrt(a * b);
+        t_next = t - p * (a_next - a) * (a_next - a);
+        p = (p << 1);
+        pi = (a_next + b_next) * (a_next + b_next) / (4 * t_next);
+
+        if (std::abs(pi - PI) < tol) {
+            myfile << i + 1 << ',' << digits << std::endl;
+            digits += 1;
+            tol = pow(10., -1. * digits);
+            // std::cout << i << " : " <<  pi - PI << std::endl;
+        }
     
-//     long unsigned i = 0;
+        a = a_next;
+        b = b_next;
+        t = t_next;
+    }
+}
 
-//     std::ofstream myfile;
-//     myfile.open(output_path);
-//     myfile << "iter,pi,tol\n";
-//     myfile.precision(20);
+void dig_ramanujan_pi(long unsigned iterations, std::string output_path) {
+    long double one_over_pi = 0;
+    long double pi;
+    double numerator, delimeter;
+    unsigned k = 0;
+    unsigned digits = 1;        // number of digits after the dot
+    long double tol = pow(10., -1. * digits);
 
-//     do {
-//         a_next = 0.5 * (a + b);
-//         b_next = sqrt(a * b);
-//         t_next = t - p * (a_next - a) * (a_next - a);
-//         p = (p << 1);
-//         pi = (a_next + b_next) * (a_next + b_next) / (4 * t_next);
+    std::ofstream myfile;
+    myfile.open(output_path);
+    myfile << "iter,digits\n";
+    myfile.precision(20);
 
-//         myfile << i + 1 << ',' << pi << ',' << tol << std::endl;
-//         if (i >= iterations)
-//             break;
-//         a = a_next;
-//         b = b_next;
-//         t = t_next;
-//         i ++;
-//     } while (std::abs(pi - PI) >  tol);
-// }
+    for (int k = 0; k < iterations; k++) {
+        unsigned fac_k = fact(k);
+        if (k <= 30) {
+            numerator = fact(4 * k) * (1103 + 26390 * k);
+            delimeter = pow(fac_k, 4) * pow(396, 4 * k);
+        }
+        one_over_pi += numerator / delimeter;
+        pi = ramanujans_const / (one_over_pi);
 
-// void dig_ramanujan_pi(long unsigned iterations, std::string output_path) {
-//     double one_over_pi = 0;
-//     double numerator, delimeter;
-//     unsigned k = 0;
+        if (std::abs(pi - PI) < tol) {
+            myfile << k + 1 << ',' << digits << std::endl;
+            digits += 1;
+            tol = pow(10., -1. * digits);
+            // std::cout << std::abs(pi - PI) << std::endl;
+        }
+    }
+}
 
-//     std::ofstream myfile;
-//     myfile.open(output_path);
-//     myfile << "iter,pi,tol\n";
-//     myfile.precision(20);
+void dig_pi_from_arctans(
+    std::vector<long double> arctan_arguments,    // аргументы арктенгенсов
+    std::vector<long double> arctan_coefs,        // коэффициенты при арктангенсах
+    long long unsigned max_iter, std::string output_path) {
 
-//     do {
-//         unsigned fac_k = fact(k);
-//         if (k <= 30) {
-//             numerator = fact(4 * k) * (1103 + 26390 * k);
-//             delimeter = pow(fac_k, 4) * pow(396, 4 * k);
-//         }
-//         one_over_pi += numerator / delimeter;
+    long double pi_div4 = 0;
+    long long unsigned_iteratons_for_arctans = 10;
+    std::vector<long double> arctan_values(arctan_arguments.size(), 0.);
+    unsigned digits = 1;        // number of digits after the dot
+    long double tol = pow(10., -1. * digits);
 
-//         myfile << k + 1 << ',' <<  ramanujans_const / (one_over_pi) << ',' << tol << std::endl;
-//         if (k >= iterations)
-//             break;
-//         k ++;
-//     } while(std::abs(ramanujans_const / one_over_pi - PI) > tol);
-// }
-
-// void dig_pi_from_arctans(
-//     std::vector<long double> arctan_arguments,    // аргументы арктенгенсов
-//     std::vector<long double> arctan_coefs,        // коэффициенты при арктангенсах
-//     long long unsigned max_iter, std::string output_path) {
-
-//     long double pi_div4 = 0;
-//     long long unsigned_iteratons_for_arctans = 10;
-//     std::vector<long double> arctan_values(arctan_arguments.size(), 0.);
-
-//     std::ofstream myfile;
-//     myfile.open(output_path);
-//     myfile << "iter,pi,tol\n";
-//     myfile.precision(20);
+    std::ofstream myfile;
+    myfile.open(output_path);
+    myfile << "iter,digits\n";
+    myfile.precision(20);
 
 
-//     // checking values for different iterations and looking at tolerance:
-//     for (unsigned iterations = 2; iterations < max_iter; iterations ++) {
-//         pi_div4 = 0;
-//         for (unsigned pos_in_vec = 0; pos_in_vec < arctan_arguments.size(); pos_in_vec ++) {
-//             arctan_values[pos_in_vec] = arctan(arctan_arguments[pos_in_vec], iterations);
-//             pi_div4 += arctan_values[pos_in_vec] * arctan_coefs[pos_in_vec];
-//         }
-//         myfile << iterations << ',' << pi_div4 * 4. << ',' << tol << std::endl;
-//         if (std::abs(4. * pi_div4 - PI) < tol)
-//             break;
-//     }
-//     myfile.close();
-//     // print_vector(arctan_values);
-// }
+    // checking values for different iterations and looking at tolerance:
+    for (unsigned iterations = 1; iterations <= max_iter; iterations ++) {
+        pi_div4 = 0;
+        for (unsigned pos_in_vec = 0; pos_in_vec < arctan_arguments.size(); pos_in_vec ++) {
+            arctan_values[pos_in_vec] = arctan(arctan_arguments[pos_in_vec], iterations);
+            pi_div4 += arctan_values[pos_in_vec] * arctan_coefs[pos_in_vec];
+        }
+        if (std::abs(pi_div4 * 4. - PI) < tol) {
+            myfile << iterations << ',' << digits << std::endl;
+            digits += 1;
+            tol = pow(10., -1. * digits);
+            // std::cout << std::abs(pi - PI) << std::endl;
+        }
+    }
+    myfile.close();
+}
 
-// void dig_viete_pi(long unsigned iterations, std::string output_path) {
-//     double a_n = sqrt(2);
-//     double two_over_pi = 1.;
-//     double pi = 1.;
+void dig_viete_pi(long unsigned iterations, std::string output_path) {
+    double a_n = sqrt(2);
+    double two_over_pi = 1.;
+    double pi = 1.;
+    unsigned digits = 1;        // number of digits after the dot
+    long double tol = pow(10., -1. * digits);
 
-//     std::ofstream myfile;
-//     myfile.open(output_path);
-//     myfile << "iter,pi,tol\n";
-//     myfile.precision(20);
+    std::ofstream myfile;
+    myfile.open(output_path);
+    myfile << "iter,digits\n";
+    myfile.precision(20);
 
-//     for (int i = 0; i < iterations; i++) {
-//         two_over_pi *= a_n / 2.;
-//         a_n = sqrt(2 + a_n);
-//         pi = 2. / two_over_pi;
-
-//         myfile << i + 1 << ',' <<  pi << ',' << tol << std::endl;
-//         if (std::abs(pi-PI) < tol)
-//             break;
-//     }
-// }
+    for (int i = 0; i < iterations; i++) {
+        two_over_pi *= a_n / 2.;
+        a_n = sqrt(2 + a_n);
+        pi = 2. / two_over_pi;
+        if (std::abs(pi - PI) < tol) {
+            myfile << i + 1 << ',' << digits << std::endl;
+            digits += 1;
+            tol = pow(10., -1. * digits);
+            // std::cout << i << " : " <<  pi - PI << std::endl;
+        }        
+    }
+}
--- CPP_labs/2nd_lab/4four_about_pi/main.cpp
+++ CPP_labs/2nd_lab/4four_about_pi/main.cpp
@@ -10,110 +10,92 @@
 #include "./includes/pi_calculations.hpp"
 #include "./includes/dig_calculation.hpp"
 
-void pring_pi_into_files(long double tolerance, long unsigned iterations) {
-
+void print_pi_into_files(long double tolerance, long unsigned iterations) {
 // ---------------Arctan methods:------------------
-// For digits I plan just manually doing it for each file
     pi_from_arctans(
         {1.},
         {1.},
         tolerance,
-        iterations, "./_files/arctan0.csv" // "./_files/arctan0.csv"
-        );
+        iterations, "./_files/arctan0.csv");
 
     pi_from_arctans(
         {1. / 2, 1. / 3},
         {1., 1.},
         tolerance,
-        iterations, "./_files/arctan1.csv" 
-        );
+        iterations, "./_files/arctan1.csv" );
 
     pi_from_arctans(
         {1. / 5, 1. / 239},
         {4., -1.},
         tolerance,
-        iterations, "./_files/arctan2.csv"
-        );
+        iterations, "./_files/arctan2.csv");
     
     pi_from_arctans(
         {1. / 8, 1. / 57, 1. / 239},
         {6., 2., 1.},
         tolerance,
-        iterations, "./_files/arctan3.csv"
-        );
+        iterations, "./_files/arctan3.csv");
 
     pi_from_arctans(
         {1. / 57, 1. / 239, 1. / 682, 1. / 12943},
         {44., 7., -12., 24.},
         tolerance,
-        iterations, "./_files/arctan4.csv"
-    );
-
+        iterations, "./_files/arctan4.csv");
 // ------------Other algorithms:-----------------------------
-
     euler_pi(tolerance, iterations, "./_files/euler.csv");
-
     gauss_legendre_pi(tolerance, iterations, "./_files/gauss.csv");
-
     ramanujan_pi(tolerance, iterations, "./_files/ramanujan.csv");
-
     leibnitz_pi(tolerance, iterations, "./_files/leibnitz.csv");
-
     viete_pi(tolerance, iterations, "./_files/viete.csv");
 }
 //------------------
 
 void print_dig_of_pi_into_files(unsigned iterations) {
-
 // ---------------Arctan methods:------------------
-// For digits I plan just manually doing it for each file
     dig_pi_from_arctans(
         {1.},
         {1.},
-        iterations, "./_files_reach_of_digits/dig_arctan0.csv"
-        );
+        iterations, "./_files_reach_of_digits/dig_arctan0.csv");
 
     dig_pi_from_arctans(
         {1. / 2, 1. / 3},
         {1., 1.},
-        iterations, "./_files_reach_of_digits/dig_arctan1.csv" 
-        );
+        iterations, "./_files_reach_of_digits/dig_arctan1.csv");
 
     dig_pi_from_arctans(
         {1. / 5, 1. / 239},
         {4., -1.},
-        iterations, "./_files_reach_of_digits/dig_arctan2.csv"
-        );
+        iterations, "./_files_reach_of_digits/dig_arctan2.csv");
     
-    dig_pi_from_arctans(
-        {1. / 8, 1. / 57, 1. / 239},
-        {6., 2., 1.},
-        iterations, "./_files_reach_of_digits/dig_arctan3.csv"
-        );
+    // dig_pi_from_arctans(
+    //     {1. / 8, 1. / 57, 1. / 239},
+    //     {6., 2., 1.},
+    //     iterations, "./_files_reach_of_digits/dig_arctan3.csv");
 
-    dig_pi_from_arctans(
-        {1. / 57, 1. / 239, 1. / 682, 1. / 12943},
-        {44., 7., -12., 24.},
-        iterations, "./_files_reach_of_digits/dig_arctan4.csv"
-    );
+    // dig_pi_from_arctans(
+    //     {1. / 57, 1. / 239, 1. / 682, 1. / 12943},
+    //     {44., 7., -12., 24.},
+    //     iterations, "./_files_reach_of_digits/dig_arctan4.csv");
 
 // ------------Other algorithms:-----------------------------
-
     dig_euler_pi(iterations, "./_files_reach_of_digits/dig_euler.csv");
-
     dig_gauss_legendre_pi(iterations, "./_files_reach_of_digits/dig_gauss.csv");
-
-    dig_ramanujan_pi(iterations, "./_files_reach_of_digits/dig_ramanujan.csv");
-
+    // dig_ramanujan_pi(iterations, "./_files_reach_of_digits/dig_ramanujan.csv");
     dig_leibnitz_pi(iterations, "./_files_reach_of_digits/dig_leibnitz.csv");
-
     dig_viete_pi(iterations, "./_files_reach_of_digits/dig_viete.csv");
 }
 
 int main() {
     std::cout << std::fixed;
-    std::cout.precision(25);
+    std::cout.precision(50);
 
-    pring_pi_into_files(-1., 50);
+    // print_pi_into_files(-1., 50);
+
+    // print_dig_of_pi_into_files(50000);
+    dig_pi_from_arctans(
+        {1. / 2, 1. / 3},
+        {1., 1.},
+        50000, "./_files_reach_of_digits/dig_arctan1.csv");
+
     return 0;
 }
