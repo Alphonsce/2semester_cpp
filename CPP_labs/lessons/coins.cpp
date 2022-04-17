a085bf33c7029ca505a51e07f50d4ef65e0c84c3
Alphonsce <varlamov.al@phystech.edu>
2022-04-17 14:07:23 +0300

    10lec is done

--- CPP_labs/lessons/coins.cpp
+++ CPP_labs/lessons/coins.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 std::vector<int> amount_of_coins(int summ, std::vector<int> coins) {
+    int n = coins.size(), cur_summ;
 
-    int n = coins.size();
+//     for (int i = 0; i < n; i++) {
+//         while (coins[i] > summ)
+//     }
 
 }
 
@@ -11,6 +15,7 @@ int main() {
     int summ = 24;
 
     std::vector<int> coins = {1, 5, 15, 20};
+    std::sort(coins.begin(), coins.end());
     
     amount_of_coins(summ, coins);
 }
