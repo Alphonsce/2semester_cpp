be4631c1ca5425aea693f0c7f4bc8417c5af3739
Alphonsce <varlamov.al@phystech.edu>
2022-05-07 20:26:10 +0300

    started writing LIS

---
+++ CPP_labs/2nd_contest/logest_incr_subseq.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int LengthOfLIS();
+
+int main() {
+    int n, num;
+    std::cin >> n;
+    int* nums = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        nums[i] = num;
+    }
+
+    delete[] nums;
+    return 0;
+}
